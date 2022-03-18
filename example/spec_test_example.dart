99870c39ff5e9b94f3a1a8363171fae562616b6d
Minsu Lee <amond@amond.net>
2022-03-18 17:00:40 +0900

    first commit

---
+++ example/spec_test_example.dart
@@ -0,0 +1,6 @@
+import 'package:spec_test/spec_test.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/spec_test.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library spec_test;
+
+export 'src/spec_test_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
